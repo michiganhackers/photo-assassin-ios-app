19cb86e1f0888888e025a55fa923a5bd258158c0
thsm <thsm@umich.edu>
2019-02-22 22:25:51 -0500

    Fix login screen to work with rotating

--- PhotoAssassin/Modules/Login/LoginScreenViewController.swift
+++ PhotoAssassin/Modules/Login/LoginScreenViewController.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 class LoginScreenViewController: UIViewController {
-    let textfieldOffset: CGFloat = 70.0
+    let titleSize: CGFloat = 56.0
     let textfieldSeparation: CGFloat = 30.0
     let loginButtonOffset: CGFloat = 100.0
 
     lazy var appTitle: UILabel = {
         let label = UILabel(frame: .zero)
         label.text = "Photo Assassin"
+        label.textAlignment = .center
         label.textColor = Colors.text
         let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: UIFont.TextStyle.headline)
-        label.font = UIFont(descriptor: descriptor, size: 36.0)
+        label.font = UIFont(descriptor: descriptor, size: titleSize)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -39,7 +42,6 @@ class LoginScreenViewController: UIViewController {
             Colors.startBackgroundGradient.cgColor,
             Colors.endBackgroundGradient.cgColor
         ]
-        gradient.frame = view.bounds
         return gradient
     }()
 
@@ -48,22 +50,28 @@ class LoginScreenViewController: UIViewController {
         view.addSubview(emailField)
         view.addSubview(passwordField)
         view.addSubview(loginButton)
+        view.layer.insertSublayer(backgroundGradient, at: 0)
+        view.backgroundColor = Colors.behindGradient
     }
 
     func setUpConstraints() {
         let margins = view.layoutMarginsGuide
 
+        appTitle.widthAnchor.constraint(lessThanOrEqualTo: margins.widthAnchor,
+                                        multiplier: 1.0).isActive = true
         appTitle.centerXAnchor.constraint(equalTo: margins.centerXAnchor).isActive = true
-        appTitle.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+        NSLayoutConstraint(item: appTitle, attribute: .top, relatedBy: .equal,
+               toItem: margins, attribute: .bottom, multiplier: 0.2,
+               constant: -50.0).isActive = true
 
         emailField.leftAnchor.constraint(equalTo: margins.leftAnchor).isActive = true
         emailField.bottomAnchor.constraint(equalTo: margins.centerYAnchor,
-            constant: -textfieldSeparation / 2.0 - textfieldOffset).isActive = true
+            constant: -textfieldSeparation / 2.0).isActive = true
         emailField.rightAnchor.constraint(equalTo: margins.rightAnchor).isActive = true
 
         passwordField.leftAnchor.constraint(equalTo: margins.leftAnchor).isActive = true
         passwordField.topAnchor.constraint(equalTo: margins.centerYAnchor,
-                    constant: textfieldSeparation / 2 - textfieldOffset).isActive = true
+                    constant: textfieldSeparation / 2).isActive = true
         passwordField.rightAnchor.constraint(equalTo: margins.rightAnchor).isActive = true
 
         loginButton.centerXAnchor.constraint(equalTo: margins.centerXAnchor).isActive = true
@@ -71,11 +79,13 @@ class LoginScreenViewController: UIViewController {
                                          constant: loginButtonOffset).isActive = true
     }
 
+    override func viewWillLayoutSubviews() {
+        setUpConstraints()
+        backgroundGradient.frame = view.bounds
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubviews()
-        setUpConstraints()
-        view.backgroundColor = Colors.behindGradient
-        view.layer.insertSublayer(backgroundGradient, at: 0)
     }
 }
