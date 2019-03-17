a3e31174187638aa8a4a3dd8864cef8ea890c121
Thomas Smith <thsm@umich.edu>
2019-03-17 02:57:26 -0400

    Left-align images in social media buttons

--- PhotoAssassin/Modules/Login/SocialMediaLoginButton.swift
+++ PhotoAssassin/Modules/Login/SocialMediaLoginButton.swift
@@ -12,6 +12,35 @@ class SocialMediaLoginButton: UIButton {
     let borderWidth: CGFloat = 4.0
     let cornerRadius: CGFloat = 15.0
     let textSize: CGFloat = 30.0
+    let leftImageMargin: CGFloat = 10.0
+
+    // This override is needed to align the social media logo to the left side
+    //  of the button. Normally, the button looks like this:
+    //    ___________________________
+    //   |        _____              |
+    //   |       |Image| Title       |   (with the title and image centered)
+    //   |       |_____|             |
+    //   |___________________________|
+    // We can calculate the amount of "remaining width" - i.e. the width of the
+    //  button unused by the image, the label, and inset margins.
+    // We can then move the image to the left by remainingWidth / 2. This left-
+    //  aligns the image, and although the label is no longer centered in the
+    //  entire container, it still is centered in the space between the image
+    //  and the right edge of the button:
+    //    ___________________________
+    //   | _____                     |
+    //   ||Image|       Title        |
+    //   ||_____|                    |
+    //   |___________________________|
+    override func imageRect(forContentRect contentRect: CGRect) -> CGRect {
+        let imageRect = super.imageRect(forContentRect: contentRect)
+        let titleRect = super.titleRect(forContentRect: contentRect)
+        let insetsWidth = imageEdgeInsets.left + imageEdgeInsets.right +
+                          titleEdgeInsets.left + titleEdgeInsets.right
+        let contentWidth = imageRect.width + titleRect.width
+        let remainingWidth = contentRect.width - insetsWidth - contentWidth
+        return imageRect.offsetBy(dx: -remainingWidth / 2.0, dy: 0.0)
+    }
 
     convenience init(_ label: String, height: CGFloat, image: UIImage) {
         self.init()
@@ -25,6 +54,8 @@ class SocialMediaLoginButton: UIButton {
         layer.borderColor = Colors.seeThroughContrast.cgColor
         layer.borderWidth = borderWidth
         layer.cornerRadius = cornerRadius
+
+        imageEdgeInsets.left = leftImageMargin
 
         translatesAutoresizingMaskIntoConstraints = false
         heightAnchor.constraint(equalToConstant: height).isActive = true
