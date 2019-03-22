04b347be6d1af445c53ac523a3c8bfd0627b8b34
Thomas Smith <thsm@umich.edu>
2019-03-21 22:10:07 -0400

    Add table view for game lobbies

---
+++ PhotoAssassin/Modules/Menu/GameLobbyList.swift
@@ -0,0 +1,30 @@
+//
+//  GameLobbyList.swift
+//  PhotoAssassin
+//
+//  Created by Thomas Smith on 3/21/19.
+//  Copyright © 2019 Michigan Hackers. All rights reserved.
+//
+
+import UIKit
+
+class GameLobbyList: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
