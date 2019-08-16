9f5643e66d20b2b7e5d335335e5d66e1f952d7e4
edwhuang <edwhuang@umich.edu>
2019-08-16 13:19:12 +0800

    working detailed game buttons

--- PhotoAssassin/Modules/Lobby/GameLobbyListCellDetailed.swift
+++ PhotoAssassin/Modules/Lobby/GameLobbyListCellDetailed.swift
@@ -13,12 +13,13 @@ class GameLobbyListCellDetailed: UITableViewCell {
     static let horizontalMargin: CGFloat = 15.0
     static let textSize: CGFloat = 36.0
     static let smallTextSize: CGFloat = 20.0
+    static let cellHeight: CGFloat = 140
     let edgeCurve: CGFloat = 12.0
-    let textSpacing: CGFloat = 15.0
+    let textSpacing: CGFloat = 5.0
     let sideSpacing: CGFloat = 10.0
     
     let faceButton = UIButton(frame: .zero)
-    let lobbyName = UITextView()
+    let lobbyName = UITextView(frame: .zero)
     let playerCountLabel = UILabel(frame: .zero)
     let targetLabel = UILabel(frame: .zero)
     let check = UIImageView(frame: .zero)
@@ -29,15 +30,12 @@ class GameLobbyListCellDetailed: UITableViewCell {
     
     init(lobby: GameLobby) {
         super.init(style: .default, reuseIdentifier: GameLobbyListCell.cellReuseIdentifer)
-        lobbyName.text = lobby.title
-        lobbyName.font = R.font.economicaBold(size: GameLobbyListCellDetailed.textSize)
-        lobbyName.textColor = Colors.text
-        lobbyName.backgroundColor = .clear
-        lobbyName.isEditable = false
-        lobbyName.isSelectable = false
-        lobbyName.frame = CGRect(x: (faceButton.frame.width / 2), y: 20, width: 150, height: 50)
-        lobbyName.translatesAutoresizingMaskIntoConstraints = false
-        faceButton.addSubview(lobbyName)
+        faceButton.setTitle(lobby.title, for: .normal)
+        faceButton.setTitleColor(Colors.text, for: .normal)
+        faceButton.titleLabel?.font = R.font.economicaBold(size: GameLobbyListCellDetailed.textSize)
+        faceButton.backgroundColor = .clear
+        faceButton.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.center;
+        faceButton.contentVerticalAlignment = UIControl.ContentVerticalAlignment.top;
         
         playerCountLabel.text = "Players remaining: \(lobby.numberInLobby)"
         playerCountLabel.textColor = Colors.text
@@ -66,35 +64,30 @@ class GameLobbyListCellDetailed: UITableViewCell {
         addConstraints()
     }
     func addButtonConstraints() {
-        lobbyName.topAnchor.constraint(equalTo: faceButton.topAnchor, constant: 20).isActive = true
-        lobbyName.centerXAnchor.constraint(equalTo: faceButton.centerXAnchor).isActive = true
-        playerCountLabel.topAnchor.constraint(equalTo: faceButton.topAnchor, constant: textSpacing).isActive = true
+        playerCountLabel.topAnchor.constraint(equalTo: faceButton.topAnchor, constant: 70).isActive = true
         playerCountLabel.leftAnchor.constraint(equalTo: faceButton.leftAnchor, constant: sideSpacing).isActive = true
         targetLabel.topAnchor.constraint(equalTo: playerCountLabel.bottomAnchor, constant: textSpacing).isActive = true
         targetLabel.leftAnchor.constraint(equalTo: faceButton.leftAnchor, constant: sideSpacing).isActive = true
-        targetLabel.bottomAnchor.constraint(equalTo: faceButton.bottomAnchor, constant: -sideSpacing).isActive = true
         check.rightAnchor.constraint(equalTo: faceButton.rightAnchor, constant: -sideSpacing).isActive = true
         check.centerYAnchor.constraint(equalTo: faceButton.centerYAnchor).isActive = true
     }
     func addConstraints() {
+        faceButton.translatesAutoresizingMaskIntoConstraints = false
         faceButton.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         faceButton.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         faceButton.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         faceButton.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         faceButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         faceButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        faceButton.translatesAutoresizingMaskIntoConstraints = false
     }
     static func getHeight() -> CGFloat {
-        return textSize * 5
+        return cellHeight
+    }
+    func isSelected() -> Bool {
+        return check.layer.opacity == 1.0
     }
     @objc
     func selected() {
-        print("selected")
-        if (check.layer.opacity == 0.0) {
-            check.layer.opacity = 1.0
-        } else {
-            check.layer.opacity = 0.0
-        }
+        check.layer.opacity = check.layer.opacity == 0.0 ? 1.0 : 0.0
     }
 }
