eab36ca13e08e4ac19119d264e64fca3ca93423b
alexzw <alexzw@umich.edu>
2019-09-26 21:35:41 -0400

    began adding more notif options. incomplete.

--- PhotoAssassin/Modules/Settings/NotificationsSettingsViewController.swift
+++ PhotoAssassin/Modules/Settings/NotificationsSettingsViewController.swift
@@ -28,17 +28,46 @@ class NotificationsSettingsViewController: NavigatingViewController {
         theSwitch.isOn = true
         return theSwitch
     }()
+    let eliminatedNotifSwitch: UISwitch = {
+        let theSwitch = UISwitch(frame: .zero)
+        theSwitch.translatesAutoresizingMaskIntoConstraints = false
+        theSwitch.isOn = true
+        return theSwitch
+    }()
+    let voteResultNotifSwitch: UISwitch = {
+        let theSwitch = UISwitch(frame: .zero)
+        theSwitch.translatesAutoresizingMaskIntoConstraints = false
+        theSwitch.isOn = true
+        return theSwitch
+    }()
+    let invitedNotifSwitch: UISwitch = {
+        let theSwitch = UISwitch(frame: .zero)
+        theSwitch.translatesAutoresizingMaskIntoConstraints = false
+        theSwitch.isOn = true
+        return theSwitch
+    }()
+    let notifsOnOffLabel = TranslucentLabel(text: "Turn all notifications on/off", size: largeTextSize)
     let overallLabel = TranslucentLabel(text: "Receive notifications when:", size: largeTextSize)
     let voteNotifLabel = TranslucentLabel(text: "You have a pending vote", size: textSize)
     let eliminationNotifLabel = TranslucentLabel(text: "Someone is eliminated", size: textSize)
+    let eliminatedNotifLabel = TranslucentLabel(text: "You are eliminated", size: textSize)
+    let voteResultNotifLabel = TranslucentLabel(text: "Voting results are in", size: textSize)
+    let invitedNotifLabel = TranslucentLabel(text: "When you are invited", size: textSize)
+    
 
     // MARK: - Custom methods
     func addSubviews() {
         view.addSubview(voteNotifSwitch)
         view.addSubview(eliminationNotifSwitch)
+        view.addSubview(eliminatedNotifSwitch)
+        view.addSubview(voteResultNotifSwitch)
+        view.addSubview(invitedNotifSwitch)
         view.addSubview(overallLabel)
         view.addSubview(voteNotifLabel)
         view.addSubview(eliminationNotifLabel)
+        view.addSubview(voteResultNotifLabel)
+        view.addSubview(invitedNotifLabel)
+        view.addSubview(eliminatedNotifLabel)
     }
     func setUpConstraints() {
         let margins = view.layoutMarginsGuide
@@ -57,6 +86,13 @@ class NotificationsSettingsViewController: NavigatingViewController {
                                              constant: leftOffset).isActive = true
         eliminationNotifSwitch.topAnchor.constraint(equalTo: eliminationNotifLabel.topAnchor).isActive = true
         eliminationNotifSwitch.rightAnchor.constraint(equalTo: margins.rightAnchor).isActive = true
+        eliminatedNotifLabel.topAnchor.constraint(equalTo: eliminationNotifLabel.bottomAnchor,
+                                                  constant: verticalSpacing).isActive = true
+        eliminatedNotifLabel.leftAnchor.constraint(equalTo: margins.leftAnchor,
+                                                   constant: leftOffset).isActive = true
+        eliminatedNotifSwitch.topAnchor.constraint(equalTo: eliminatedNotifLabel.topAnchor).isActive = true
+        eliminatedNotifSwitch.rightAnchor.constraint(equalTo: margins.rightAnchor).isActive = true
+
     }
 
     // MARK: - Event listeners
