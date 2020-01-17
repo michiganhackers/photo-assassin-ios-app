261604fa4b199f3fd7b40f9195b0e5ae6dda141b
Cole Riggle <coleriggle@gmail.com>
2020-01-16 19:39:23 -0500

    Recreated deleted files

---
+++ PhotoAssassin/Modules/Profile/PictureChangeCameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePictureCameraViewController.swift
+//  PhotoAssassin
+//
+//  Created by Cole Riggle on 1/16/20.
+//  Copyright © 2020 Michigan Hackers. All rights reserved.
+//
+
+import UIKit
+
+class PictureChangeCameraViewController: BaseCameraViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.gestureRecognizers?.removeAll()
+        
+    }
+}
