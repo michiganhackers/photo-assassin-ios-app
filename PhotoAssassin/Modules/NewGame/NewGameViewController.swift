3af324e51107a6ed798bb9a74583a5eefc945313
leonhadi <leonhadi@umich.edu>
2020-02-06 20:53:13 -0500

    error fixing

--- PhotoAssassin/Modules/NewGame/NewGameViewController.swift
+++ PhotoAssassin/Modules/NewGame/NewGameViewController.swift
@@ -13,10 +13,7 @@ class NewGameViewController: NavigatingViewController, UITextFieldDelegate {
     let mainTextSize: CGFloat = 36.0
     let verticalSpacing: CGFloat = 30.0
     let backend = BackendCaller()
-//    let pickerData = ["1", "2", "3", "4", "5", "6", "7", "8", "10"]
-    
     // MARK: - UI Elements
-    
     lazy var titleLabel: UILabel = {
         let titleLabel = UILabel()
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -31,13 +28,11 @@ class NewGameViewController: NavigatingViewController, UITextFieldDelegate {
 
     lazy var titleTextField: UITextField = {
         let titleText = UserEnterTextField("")
-        titleText.delegate = self
+//        titleText.delegate = self
         titleText.translatesAutoresizingMaskIntoConstraints = false
         titleText.returnKeyType = .done
         return titleText
     }()
-    
-    
     lazy var playerLimit: UILabel = {
         let playerLimit = UILabel()
         playerLimit.translatesAutoresizingMaskIntoConstraints = false
@@ -49,15 +44,14 @@ class NewGameViewController: NavigatingViewController, UITextFieldDelegate {
         )
         return playerLimit
     }()
-    
     lazy var playerLimitTextField: UITextField = {
-        let playerNumber = UserEnterTextField("", height: 50)
+        let playerNumber = UserEnterTextField("", height: 45)
         playerNumber.delegate = self
         playerNumber.returnKeyType = .done
         playerNumber.translatesAutoresizingMaskIntoConstraints = false
+        playerNumber.keyboardType = .numberPad
         return playerNumber
     }()
-
     lazy var invitePlayersButton: UIButton = {
         let button = TransparentButton("Invite Players")
         button.addTarget(self, action: #selector(invitePlayersButtonTapped), for: .touchUpInside)
@@ -83,13 +77,31 @@ class NewGameViewController: NavigatingViewController, UITextFieldDelegate {
     // MARK: - Custom Functions
     @objc
     func bringToCreate() {
-        if let name = titleTextField.text {
+        guard let text: String = playerLimitTextField.text, let playerCount = Int(text), playerCount >= 3 else  {
+                let alertTitle = " Error "
+                let alertText  = " Have to be at least 3 Players "
+                let alertVC = UIAlertController(title: alertTitle, message: alertText, preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+            return
+                }
+        guard let name = titleTextField.text, name != "" else {
+            let alertTitle = " Error "
+            let alertText = " Please Type a Valid Game Title "
+            let alertVC = UIAlertController(title: alertTitle, message: alertText, preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+            return
+        }
             let invitedPlayersList = Array(invitedPlayers)
-            print("TODO: Create game with name \(name) and invited players \(invitedPlayersList)")
             backend.createGame(name: name, invitedUsernames: invitedPlayersList) { result, error in
                 if let actualError = error {
                     print("Encountered error when creating game:\n\(actualError)")
-                    // TODO: Show error to user
+                    let alertTitle = " Error "
+                    let alertText  = " could not create game, try again later "
+                    let alertVC = UIAlertController(title: alertTitle, message: alertText, preferredStyle: .alert)
+                    alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alertVC, animated: true, completion: nil)
                 }
                 guard let gameID = result else {
                     print("No gameID passed back but game creation seemed succesful")
@@ -101,9 +113,7 @@ class NewGameViewController: NavigatingViewController, UITextFieldDelegate {
             }
             invitedPlayers = Set<String>()
             titleTextField.text = ""
-        }
     }
-
     @objc
     func invitePlayersButtonTapped() {
         if let nav = navigationController {
@@ -147,10 +157,25 @@ class NewGameViewController: NavigatingViewController, UITextFieldDelegate {
         view.addSubview(playerLimitTextField)
     }
        // MARK: - Picker View
-    
-    
-    
-    func textFieldShouldReturn( textField: UITextField) -> Bool {
+    enum ValidationError: Error {
+            case moreThanFifty(String)
+    }
+    func errorCatch () throws {
+        throw ValidationError.moreThanFifty("Too Many Players")
+    }
+    func textField(textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+      guard !string.isEmpty else {
+          return true
+      }
+        if string.rangeOfCharacter(from: NSCharacterSet.decimalDigits) != nil {
+          return true
+       } else {
+          return false
+       }
+    }
+    func textFieldShouldReturn( _ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
